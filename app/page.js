4f9748626db2b754ca20da036fc55f09d3c99d4d
sanaz niamati <93905446+sanazniamati@users.noreply.github.com>
2023-12-25 10:01:54 +0330

    feat:added vscode setting

--- app/page.js
+++ app/page.js
@@ -4,5 +4,10 @@ import Image from "next/image";
 import styles from "./page.module.css";
 
 export default function Home() {
-  return <h1>this is demo for using ant design</h1>;
+  return (
+    <div>
+      <h1>this is demo for using ant design</h1>
+      <div>sanaz</div>
+    </div>
+  );
 }
