1f0a29a7822d05ddb8a5f872c25bb48fb5716502
sanaz niamati <93905446+sanazniamati@users.noreply.github.com>
2023-12-25 15:59:28 +0330

    build:remove prettierignor and prettierrc

--- app/page.js
+++ app/page.js
@@ -6,7 +6,7 @@ import styles from "./page.module.css";
 export default function Home() {
   return (
     <div>
-      <h1>this is demo for using ant design</h1>
+      <h1>this is demo f or using ant design</h1>
       <div>sanaz</div>
     </div>
   );
